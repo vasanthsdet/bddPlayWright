1239f40c2d26d1c1ad0f3597e0d97ae933a7e7b7
vasanthsdet <dama.vasanth@gmail.com>
2024-08-12 19:15:28 +0530

    Added workflow job

--- src/test/java/com/bddPlayWright/hooks/TestHooks.java
+++ src/test/java/com/bddPlayWright/hooks/TestHooks.java
@@ -1,8 +1,13 @@
 package com.bddPlayWright.hooks;
 
 import com.bddPlayWright.base.BaseTest;
+import com.microsoft.playwright.Page;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+import java.nio.file.Paths;
 
 public class TestHooks {
     private final BaseTest baseTest;
@@ -23,5 +28,19 @@ public class TestHooks {
 
     public BaseTest getBaseTest() {
         return baseTest;
+    }
+
+    @AfterStep
+    public void takeScreenshotAfterStep(Scenario scenario) {
+        // Create a unique file name based on scenario and step
+        String screenshotName = scenario.getName().replaceAll(" ", "_") + "_" + scenario.getStatus() + ".png";
+        String screenshotPath = "target/screenshots/" + screenshotName;
+
+        // Take the screenshot using Playwright and save it
+        Page page = baseTest.getPage(); // Ensure the page object is accessible
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(screenshotPath)));
+
+        // Attach the screenshot to the report
+        scenario.attach(page.screenshot(), "image/png", screenshotName);
     }
 }
